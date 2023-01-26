aa8c32b09c6f7100840e9963c87e001edfba8480
Linho <106666571+Purolinho@users.noreply.github.com>
2023-01-26 13:24:18 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+var url = "https://v2.jokeapi.dev/joke/Any?type=single"
+var piada = document.getElementById("piada")
+
+async function getjoke() {
+    fetch(url)
+    .then(data => data.json())
+    .then(item => {
+        piada.textContent = item.joke
+    })
+}
+
+var botao = document.getElementById("botao")
+botao.addEventListener("click", () => {
+    getjoke()
+    piada.style.transition = "0s"
+    piada.style.opacity = 0
+    setTimeout(() => {
+    piada.style.transition = "1s"
+    piada.style.opacity = 1
+    }, 100);
+})
